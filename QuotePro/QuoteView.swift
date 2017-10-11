4ac5cffa27ba3d1d83b7c1309cbd35d6e2622875
Paul <paul.huynh3@gmail.com>
2017-10-11 12:30:53 -0400

    starter

---
+++ QuotePro/QuoteView.swift
@@ -0,0 +1,24 @@
+//
+//  QuoteView.swift
+//  QuoteProStarter
+//
+//  Created by Paul on 2017-10-11.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+import UIKit
+
+class QuoteView: UIView {
+
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    
+    
+    
+
+}
